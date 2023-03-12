0e5a44c6bfae1346600abdfb9854abd0e2bb677a
Mateus Alves Conceição <mateus.alvesc02@gmail.com>
2023-03-12 15:27:47 -0300

    mensageria com RabbitMQ

--- pagamentos/src/main/java/br/com/alurafood/pagamentos/config/Configuration.java
+++ pagamentos/src/main/java/br/com/alurafood/pagamentos/config/Configuration.java
@@ -2,10 +2,9 @@ package br.com.alurafood.pagamentos.config;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
-public class Configuracao {
+@org.springframework.context.annotation.Configuration
+public class Configuration {
 
     @Bean
     public ModelMapper obterModelMapper() {
